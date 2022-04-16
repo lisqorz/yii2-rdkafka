b51236ef16fd3b4635559975e390d941b2020e2c
lisqorz <mail@lishunqi.com>
2022-04-16 19:14:12 +0800

    .

--- src/DependencyInjection.php
+++ src/DependencyInjection.php
@@ -3,7 +3,6 @@
 namespace lisqorz\rdkafka;
 
 use lisqorz\rdkafka\controller\KafkaController;
-use yii\base\Application;
 use yii\base\BootstrapInterface;
 
 class DependencyInjection implements BootstrapInterface
@@ -11,32 +10,17 @@ class DependencyInjection implements BootstrapInterface
 
     public function bootstrap($app)
     {
-        echo 123;
-        exit;
         $this->addControllers($app);
     }
+
     /**
-     * Auto-configure console controller classes
-     * @param Application $app
+     * 添加控制器到console
+     * @param $app
      */
-    private function addControllers(Application $app)
+    private function addControllers($app)
     {
-//        $config = $app->kafka->getConfig();
-        $app->controllerMap['kafka'] = KafkaController::class;
+        if ($app instanceof \yii\console\Application) {
+            $app->controllerMap["kafka"] = KafkaController::class;
+        }
     }
-//
-//    /**
-//     * Register connections in service container
-//     * @param Configuration $config
-//     */
-//    protected function registerConnections(Configuration $config)
-//    {
-//        foreach ($config->connections as $options) {
-//            $serviceAlias = sprintf(Configuration::CONNECTION_SERVICE_NAME, $options['name']);
-//            \Yii::$container->setSingleton($serviceAlias, function () use ($options) {
-//                $factory = new AbstractConnectionFactory($options['type'], $options);
-//                return $factory->createConnection();
-//            });
-//        }
-//    }
 }
